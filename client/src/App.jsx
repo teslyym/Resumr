96320f59ef94b7eeb39dbcb6e4127d8138067853
Teslim <steslimolayiwola@gmail.com>
2026-05-02 18:39:04 +0100

    feat: implement routing with Home component in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
