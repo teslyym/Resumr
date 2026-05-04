8437aa9c038faeb7f0e6703011fb75a29c699bf9
Teslim <steslimolayiwola@gmail.com>
2026-05-04 21:31:29 +0100

    add CV routes for CRUD operations with authentication

---
+++ server/routes/cvRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createCV,
+  getMyCVs,
+  getCVById,
+  updateCV,
+  deleteCV,
+} = require("../controllers/cvController");
+const { protect } = require("../middleware/authMiddleware");
+
+router.use(protect);
+
+router.post("/", createCV);
+router.get("/", getMyCVs);
+router.get("/:id", getCVById);
+router.put("/:id", updateCV);
+router.delete("/:id", deleteCV);
+
+module.exports = router;
