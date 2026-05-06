3385cc2637114be602bf2884ecba82b97e597481
Teslim <steslimolayiwola@gmail.com>
2026-05-06 13:24:45 +0100

    update token expiration to 24 hours for improved security

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -6,8 +6,9 @@ const jwt = require("jsonwebtoken");
  */
 const generateToken = (res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    expiresIn: process.env.JWT_EXPIRES_IN || "24h",
   });
+
 
   res.cookie("jwt", token, {
     httpOnly: true, // JS can't read it (XSS protection)
